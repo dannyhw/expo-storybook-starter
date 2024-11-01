ba4c0667d2fd9516435208edbabe9da0fd9178e9
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-11-01 14:27:29 +0000

    fix: update version

--- .ondevice/storybook.requires.ts
+++ .ondevice/storybook.requires.ts
@@ -31,7 +31,7 @@ declare global {
 const annotations = [
   require("./preview"),
   require("@storybook/react-native/preview"),
-  require("@storybook/addon-actions/preview"),
+  require("@storybook/addon-ondevice-actions/preview"),
 ];
 
 global.STORIES = normalizedStories;
