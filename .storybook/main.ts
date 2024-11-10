cf0776979d5203019d6e01fcc291d884c44e1b6a
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-11-10 11:30:20 +0000

    feat: use vite for web

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,14 @@
-/** @type{import("@storybook/react-webpack5").StorybookConfig} */
+/** @type{import("@storybook/react-native-web-vite").StorybookConfig} */
 module.exports = {
   stories: [
     "../components/**/*.stories.mdx",
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
 
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-webpack5-compiler-babel",
-    "@chromatic-com/storybook",
-    "@storybook/addon-react-native-web",
-  ],
+  addons: ["@storybook/addon-essentials", "@chromatic-com/storybook"],
 
   framework: {
-    name: "@storybook/react-webpack5",
+    name: "@storybook/react-native-web-vite",
     options: {},
   },
 
