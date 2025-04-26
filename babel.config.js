afbca82c938f5dc657285cd036f576edb2871f82
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-04-26 15:42:30 +0100

    feat: v9 update

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
+      "@babel/plugin-transform-class-static-block",
       ["babel-plugin-react-docgen-typescript", { exclude: "node_modules" }],
     ],
   };
