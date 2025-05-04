b5fbc2e07d68978ca9419cd7f97253b11c4a24cb
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-05-04 17:56:11 +0100

    fix

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "@babel/plugin-transform-class-static-block",
       ["babel-plugin-react-docgen-typescript", { exclude: "node_modules" }],
       "react-native-reanimated/plugin",
     ],
